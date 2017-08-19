ebd5ac6776e60cad93709212c82be77359d2027a
Charly <charly.strn@gmail.com>
2017-08-19 19:27:14 +0200

    Script permis de conduire réparé

---
+++ server-data/resources/[system]/client_base/__resource.lua
@@ -0,0 +1,25 @@
+-- Client Base Scripts
+
+
+client_script 'pointing.lua'
+client_script 'neverwanted.lua'
+client_script 'handsup.lua'
+client_script 'vocalchat.lua'
+client_script 'stamina.lua'
+client_script 'sirencontrols.net.dll'
+
+
+
+client_script 'carhud.lua'
+
+
+
+ui_page('lock/client/html/index.html')
+
+
+files({
+    'lock/client/html/index.html',
+    'lock/client/html/sounds/lock.ogg',
+    'lock/client/html/sounds/unlock.ogg',
+    'lock/client/html/sounds/demo.ogg'
+})
